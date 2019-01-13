f91bfc77c2a53cf0a154be652cbba9827d12c58a
tom-votes <tom.votes.rla@gmail.com>
2019-01-13 16:21:33 -0500

    Add cosmetic fixes before merging Zara's code

--- ui/rla_ui.js
+++ ui/rla_ui.js
@@ -471,7 +471,12 @@ function displayAuditStatus(andThen) {
          finalResultContainer.removeChild(progressBar);
          finalResultContainer.innerHTML = 'Audit status:';
          msg['outcomes'].forEach(function(outcome) {
-            finalResultContainer.innerHTML += '<p>Contest: <strong>'+outcome['contest_id']+'</strong> Status: <strong>'+outcome['status']+'</strong> ('+outcome.progress+')</p>';
+            // 'all_contests' should probably be a dictionary so we don't need to do this filter[0]:
+            var contest = conductorState['all_contests'].filter(function(x) {
+               return x['id'] == outcome['contest_id'];
+            })[0];
+//OUT
+            finalResultContainer.innerHTML += '<p>Contest: <strong>'+contest['title']+'</strong> Status: <strong>'+outcome['status']+'</strong> ('+outcome.progress+')</p>';
          });
          finalResultContainer.innerHTML += '<br /><br /><a href="/reset">Reset and audit another contest</a>';
          finalResultContainer.style.display = 'block';
